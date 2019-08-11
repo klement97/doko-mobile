805b512ac9438292b18a28956e71c24c63bb4d2b
Klement Omeri <klement-omeri97@hotmail.com>
2019-08-11 12:32:25 +0200

    refactoring BASE_URL

--- mobile/static/js/index.js
+++ mobile/static/js/index.js
@@ -1,4 +1,4 @@
-BASE_URL = 'http://localhost:8000';
+BASE_URL = 'https://dokomobile.herokuapp.com';
 
 function isValid(email) {
     let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
