97ff4ee91eb6997a95f16ea19a9cdb39c5db0d1a
Klement Omeri <klement-omeri97@hotmail.com>
2019-08-11 19:01:51 +0200

    refactoring

--- dokomobile/settings.py
+++ dokomobile/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ogbh-mrc=huy*v4o)plze%(iz)hslr*t7=!=uz##nmqyi#-gs='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
