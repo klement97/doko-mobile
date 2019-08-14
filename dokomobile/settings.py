74634bfa977e065f9e8a9259e4d60c720284cf91
Klement Omeri <klement-omeri97@hotmail.com>
2019-08-15 00:21:48 +0200

    refactoring master settings

--- dokomobile/settings.py
+++ dokomobile/settings.py
@@ -125,12 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
-#
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'),
-# )
 
 LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/manager'
