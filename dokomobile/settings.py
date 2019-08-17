0d8e613309f59efd1cf951cd63e5d42fdea74209
Klement Omeri <klement-omeri97@hotmail.com>
2019-08-17 14:02:51 +0200

    refactoring master settings

--- dokomobile/settings.py
+++ dokomobile/settings.py
@@ -128,7 +128,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'mobile/static'),
 )
 # web: python manage.py runserver 0.0.0.0:$PORT
 
